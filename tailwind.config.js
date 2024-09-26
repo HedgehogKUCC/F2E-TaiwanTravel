24b2beb6eb1f158b030b5cc57aee024248d2eaf1
CHAN-CHENG KU <ccc908925@gmail.com>
2024-09-26 17:10:43 +0800

    add: tailwind.config.js
    基本設定和客製顏色

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors: {
+      primary: {
+        300: '#5B9BD5',
+        500: '#00457C'
+      },
+      yellow: '#E8D4A2',
+      pink: '#D69F8F',
+      grey: {
+        300: '#F5F4F2',
+        500: '#F1EDEA'
+      },
+      dark: '#000000',
+      white: '#FFFFFF'
+    }
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: []
+}
