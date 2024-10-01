ce311d5bb78c8dc895fced35ebba38c1e8c6ba6b
CHAN-CHENG KU <ccc908925@gmail.com>
2024-10-01 13:51:18 +0800

    config: Removing unused CSS
    tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,40 @@
 module.exports = {
-  purge: [],
+  purge: {
+    content: [
+      './components/**/*.{vue,js}',
+      './layouts/**/*.vue',
+      './pages/**/*.vue',
+      './plugins/**/*.{js,ts}',
+      './nuxt.config.{js,ts}'
+    ],
+    options: {
+      keyframes: true
+    }
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    colors: {
-      primary: {
-        300: '#5B9BD5',
-        500: '#00457C'
-      },
-      yellow: '#E8D4A2',
-      pink: '#D69F8F',
-      grey: {
-        300: '#F5F4F2',
-        500: '#F1EDEA'
-      },
-      dark: '#000000',
-      white: '#FFFFFF'
+    extend: {
+      colors: {
+        primary: {
+          300: '#5B9BD5',
+          500: '#00457C'
+        },
+        yellow: '#E8D4A2',
+        pink: '#D69F8F',
+        grey: {
+          300: '#F5F4F2',
+          500: '#F1EDEA'
+        },
+        dark: '#000000',
+        white: '#FFFFFF'
+      }
+    },
+    container: {
+      center: true
     }
   },
   variants: {
-    extend: {}
+    extend: {},
   },
   plugins: []
 }
