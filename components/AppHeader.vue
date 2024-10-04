bdeabd24122c92bde8e41bfb06bee891ffbbfdab
CHAN-CHENG KU <ccc908925@gmail.com>
2024-10-04 15:37:34 +0800

    feat: AppHeader.vue

---
+++ components/AppHeader.vue
@@ -0,0 +1,17 @@
+<template>
+  <header class="bg-primary-500">
+    <div class="container py-4 flex justify-between items-center">
+      <img src="~/assets/images/logo.png" alt="">
+      <ul class="flex justify-end items-center">
+        <li class="mr-5 text-white">精選活動</li>
+        <li class="mr-5 text-white">全台景點</li>
+        <li class="mr-5 text-white">探索美食</li>
+        <li class="text-white">住宿飯店</li>
+      </ul>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {}
+</script>
---
+++ layouts/default.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <AppHeader />
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
