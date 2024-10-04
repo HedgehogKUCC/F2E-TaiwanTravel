706548ec7fc43138f74192e5433bbdb8d901e001
CHAN-CHENG KU <ccc908925@gmail.com>
2024-10-04 16:08:26 +0800

    feat: AppFooter.vue

---
+++ components/AppFooter.vue
@@ -0,0 +1,14 @@
+<template>
+  <footer class="bg-black py-10 text-center">
+    <p class="mb-1 text-white">
+      Copyright@2024 KUCC 保留一切權利。
+    </p>
+    <p class="text-white">
+      Designed by Alena Hsu
+    </p>
+  </footer>
+</template>
+
+<script>
+export default {}
+</script>
